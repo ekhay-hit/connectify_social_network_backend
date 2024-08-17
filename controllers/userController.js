1c106b0743412653483f0e969a1288886218dd7f
Houssine <ekhay1@outlook.com>
2024-08-17 17:08:40 -0400

    added backend get request to get a user by id

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,4 +25,22 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+
+  // get a singel user based on its id
+
+  async getUserById(req, res) {
+    try {
+      const user = await User.findById(req.params.id);
+
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: "No user with the associated id" });
+      }
+
+      res.status(200).json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,8 +1,13 @@
 const router = require("express").Router();
-const { getUsers, createUser } = require("../../controllers/userController.js");
+const {
+  getUsers,
+  createUser,
+  getUserById,
+} = require("../../controllers/userController.js");
 
 // the url /api/user/
 //chaining get and post request base on the url
 router.route("/").get(getUsers).post(createUser);
+router.route("/:id").get(getUserById);
 
 module.exports = router;
