1e1328133e27027ffab24c409f0164b55f369fe0
Houssine <ekhay1@outlook.com>
2024-08-17 19:50:00 -0400

    added hooks to remove thoughts associated with the user before removing a user

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const Thought = require("./Thought.js");
 
 // schema to create User model
 const userSchema = new Schema(
@@ -38,6 +39,15 @@ const userSchema = new Schema(
     id: false,
   }
 );
+
+// hook that remove the associated thought before removing any user
+userSchema.pre("remove", async function (next) {
+  const user = this;
+
+  await Thought.deleteMany({ user: user._id });
+
+  next();
+});
 
 //initializing User model
 const User = model("user", userSchema);
